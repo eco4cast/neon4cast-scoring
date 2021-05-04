b2b1b95408784254e39e928d79b1c8a92ee1b3b7
rqthomas <rqthomas@vt.edu>
2021-05-04 13:57:43 +0000

    cleaning up scoring repo

--- scoring.R
+++ scoring.R
@@ -1,8 +1,9 @@
 #remotes::install_deps()
 
 library(aws.s3)
+library(neon4cast)
 
-source("R/score_it.R")
+#source("R/score_it.R")
 source("R/download_bucket.R")
 source("../neon4cast-shared-utilities/publish.R")
 
@@ -21,11 +22,11 @@ targets_file <- targets[grepl("aquatics-targets", targets)]
 forecast_files <- forecasts[grepl("aquatics-", forecasts)]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
-score_files <- score_it(targets_file, forecast_files,
+score_files <- neon4cast:::score_it(targets_file, forecast_files,
          target_variables = c("oxygen", "temperature"))
 
 ## Publish
-publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
+publish(code = c("R/download_bucket.R"),
         data_in = c(targets_file, forecast_files),
         data_out = score_files,
         prefix = "aquatics/",
@@ -38,11 +39,11 @@ targets_file <- targets[grepl("beetles-", targets)]
 forecast_files <- forecasts[grepl("beetles-", forecasts)]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
-score_files <- score_it(targets_file, forecast_files,
+score_files <- neon4cast:::score_it(targets_file, forecast_files,
                        target_variables = c("richness", "abundance"),
                        )
 
-publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
+publish(code = c("R/download_bucket.R"),
         data_in = c(targets_file, forecast_files),
         data_out = score_files,
         prefix = "beetles/",
@@ -55,10 +56,10 @@ targets_file <- targets[grepl("terrestrial_daily-", targets)]
 forecast_files <- forecasts[grepl("terrestrial_daily-", forecasts)]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
-score_files <- score_it(targets_file, forecast_files,
+score_files <- neon4cast:::score_it(targets_file, forecast_files,
                         target_variables = c("nee", "le", "vswc"))
 
-publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
+publish(code = c("R/download_bucket.R"),
         data_in = c(targets_file, forecast_files),
         data_out = score_files,
         prefix = "terrestrial/",
@@ -67,19 +68,19 @@ publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
 
 
 ## terrestrial
-targets_file <- targets[grepl("terrestrial_30min-", targets)]
-forecast_files <- forecasts[grepl("terrestrial_30min-", forecasts)]
-forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
-forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
-score_files <- score_it(targets_file, forecast_files,
-                        target_variables = c("nee", "le", "vswc"))
+#targets_file <- targets[grepl("terrestrial_30min-", targets)]
+#forecast_files <- forecasts[grepl("terrestrial_30min-", forecasts)]
+#forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
+#forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
+#score_files <- neon4cast:::score_it(targets_file, forecast_files,
+#                        target_variables = c("nee", "le", "vswc"))
 
-publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
-        data_in = c(targets_file, forecast_files),
-        data_out = score_files,
-        prefix = "terrestrial/",
-        bucket = "scores",
-        registries = "https://hash-archive.carlboettiger.info")
+#publish(code = c("R/download_bucket.R"),
+#        data_in = c(targets_file, forecast_files),
+#        data_out = score_files,
+#        prefix = "terrestrial/",
+#        bucket = "scores",
+#        registries = "https://hash-archive.carlboettiger.info")
 
 
 ## phenology
@@ -87,10 +88,10 @@ targets_file <- targets[grepl("phenology-", targets)]
 forecast_files <- forecasts[grepl("phenology-", forecasts)]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
-score_files <- score_it(targets_file, forecast_files,
+score_files <- neon4cast:::score_it(targets_file, forecast_files,
                         target_variables = c("gcc_90"))
 
-publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
+publish(code = c("R/download_bucket.R"),
         data_in = c(targets_file, forecast_files),
         data_out = score_files,
         prefix = "phenology/",
@@ -102,12 +103,12 @@ targets_file <- targets[grepl("ticks-", targets)]
 forecast_files <- forecasts[grepl("ticks-", forecasts)]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "xml")]
 forecast_files <- forecast_files[!stringr::str_detect(forecast_files, "not_in_standard")]
-score_files <- score_it(targets_file, forecast_files,
-                        target_variables = c("Ixodes_scapularis", "Ambloyomma_americanum"),
+score_files <- neon4cast:::score_it(targets_file, forecast_files,
+                        target_variables = c("ixodes_scapularis", "amblyomma_americanum"),
                         grouping_variables = c("time", "plotID", "siteID"))
 
 
-publish(code = c("scoring.R","R/score_it.R", "R/download_bucket.R"),
+publish(code = c("R/download_bucket.R"),
         data_in = c(targets_file, forecast_files),
         data_out = score_files,
         prefix = "ticks/",
