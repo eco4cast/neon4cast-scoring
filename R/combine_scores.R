4ee7433c050db92aa0667c5d45c2a9f32e5cbf98
rqthomas <rqthomas@vt.edu>
2021-12-11 03:42:21 +0000

    score schema

--- R/combine_scores.R
+++ R/combine_scores.R
@@ -5,24 +5,7 @@ combined <- readr::read_csv(scores_files, progress = FALSE)
 ## Remote arrow:
 library(arrow)
 library(dplyr)
-s <- arrow::schema(
-  theme = string(),
-  team = string(),
-  issue_date = date32(),
-  siteID = string(),
-  time = arrow::timestamp("s", timezone="UTC"),
-  target = string(),
-  mean = float64(),
-  sd = float64(),
-  observed = float64(),
-  crps = float64(),
-  logs = float64(),
-  upper95 = float64(),
-  lower95 = float64(),
-  interval = int64(),
-  forecast_start_time = timestamp("s", timezone="UTC"),
-  horizon = float64()
-)
+s <- neon4cast::score_schema()
 s3 <- s3_bucket(bucket = "scores", endpoint_override = "data.ecoforecast.org")
 ds <- open_dataset("scores", schema=s, format = "csv", skip_rows = 1)
 
