9c32a300674fdea848c8272bb76e2dccb81f8e31
rqthomas <rqthomas@vt.edu>
2021-12-14 00:25:26 +0000

    remote only

--- R/combine_scores.R
+++ R/combine_scores.R
@@ -7,12 +7,14 @@ library(arrow)
 library(dplyr)
 s <- neon4cast::score_schema()
 s3 <- s3_bucket(bucket = "scores", endpoint_override = "data.ecoforecast.org")
-ds <- open_dataset("scores", schema=s, format = "csv", skip_rows = 1)
+ds <- open_dataset(s3, schema=s, format = "csv", skip_rows = 1)
 
-## Simpler format fails due to date vs datetime formatting of "time"
-#ds <- open_dataset(s3, format = "csv")
 
 ## Test
 bet <- ds %>% filter(theme == "beetles") %>% collect()
+
+
+## Simpler format fails due to date vs datetime formatting of "time"
+#ds <- open_dataset(s3, format = "csv")
 
 
